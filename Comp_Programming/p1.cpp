049d7d0b3b3f82c466a950298a6897d135b223e5
manyuvreddy <113624806+manyuvreddy@users.noreply.github.com>
2025-11-23 17:40:21 +0530

    xcvxcvxcv

---
+++ Comp_Programming/p1.cpp
@@ -0,0 +1,12 @@
+// output given number in reverse order, all the digits separately and together
+
+# include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+    cin>>x,y;
+    z=x+y;
+    cout<<z<<endl;
+    return 0;
+}
